9c42e09f7d8296f7d14783900859e340cf09b7ba
Enrico Sponholz Barreto <enricosbarreto@gmail.com>
2026-02-24 17:44:11 -0300

    feat: modularize database and logic into separate files

--- index.ts
+++ index.ts
@@ -1,26 +1,4 @@
-interface Movie {
-
-    title: string;
-
-    watched: boolean;
-
-    rating?: number;
-
-}
-
-const myCatalog: Movie[] = [
-
-  { title: "Truman's show", watched: true, rating: 8},
-
-  {title: "legally blond", watched: true, rating: 4.5},
-
-  {title: "a morte te da parabens", watched: false},
-
-  {title: "nada de novo no front", watched: false},
-
-  {title: "The usual suspects", watched: false}
-
-];
+import {Movie, myCatalog} from './database'
 
 
 

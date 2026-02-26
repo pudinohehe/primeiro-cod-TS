f4a7632eadf3f62291e2668bbd38d08f86a87448
Enrico Sponholz Barreto <enricosbarreto@gmail.com>
2026-02-26 19:10:27 -0300

    refactor: move logic to functions.ts to decouple from index.ts
    - Separated movie logic and utility functions for better organization.
    
    - Implemented duplicate check and early return in addNewMovie.

---
+++ functions.ts
@@ -0,0 +1,49 @@
+import readline from "readline-sync";
+import { Movie } from "./types";
+import { myCatalog } from ".";
+import fs from "fs";
+
+export function addNewMovie() {
+    const newTitle = readline.question("What is the name of the movie? ");
+    const alreadyWatched: boolean = readline.keyInYNStrict("Did you watch the movie? ");
+    const alreadyExist = myCatalog.some(
+    (movie) => movie.title.toLowerCase() === newTitle.toLowerCase()
+    )
+    if (alreadyExist){
+
+        console.log("Movie is already on the list")
+        return
+    }
+    const newMovie: Movie = {
+        title: newTitle,
+        watched: alreadyWatched
+    };
+    
+    if (alreadyWatched){
+         const rate = Number(readline.question("what's the movie rate?"))
+         newMovie.rating = rate;
+    }
+    myCatalog.push(newMovie);
+    saveonJson();
+
+}
+
+export function markAsWatched(movieTitle: string) {
+  const rightMovie = myCatalog.find(
+    (movie) => movie.title.toLowerCase() === movieTitle.toLowerCase(),
+  );
+  if (rightMovie) {
+    let rightMovieRate = Number(readline.question("what's the movie rate?"));
+    rightMovie.watched = true;
+    rightMovie.rating = rightMovieRate;
+    console.log(`Movie now is maked as seen`);
+    saveonJson();
+  } else {
+    console.log(`Movie is not on the list`);
+  }
+}
+
+export function saveonJson(){
+    const dataSave = JSON.stringify(myCatalog, null, 2);
+    fs.writeFileSync("./database.json", dataSave);
+}

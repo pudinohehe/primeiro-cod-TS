3c63885cdde248c154580d27ba864b9163acf765
Enrico Sponholz Barreto <enricosbarreto@gmail.com>
2026-02-26 19:47:57 -0300

    feat: organize project structure and add resilience with try/catch
    - Moved source files to src/ and database to data/
    
    - Implemented try/catch block to handle missing or corrupt database.json

---
+++ src/src/index.ts
@@ -0,0 +1,57 @@
+import readline from "readline-sync";
+import { Movie } from "./types";
+import fs from "fs";
+import { saveonJson, addNewMovie, markAsWatched } from "./functions";
+
+export let myCatalog: Movie[] = [];
+
+try{
+const rawdata = fs.readFileSync("./data/database.json", "utf-8");
+myCatalog = JSON.parse(rawdata);
+} catch{
+  myCatalog = [];
+  console.log("No database found. Starting a new one!")
+}
+
+
+let exit = false;
+
+while (!exit) {
+  console.clear();
+  const menu = Number(
+    readline.question(
+      "Choose between this options:\n1. Add Movie \n2. Mark as Watched \n3. Show movies to watch \n4. Show the watched catalog \n5. Exit \n",
+    ),
+  );
+  switch (menu) {
+    case 1:
+      addNewMovie();
+      break;
+    case 2:
+      const userAnswer = readline.question("Wich film did you watch?");
+      markAsWatched(userAnswer);
+      break;
+    case 3:
+      const toSee = myCatalog.filter((movie) => {
+        return movie.watched === false;
+      });
+
+      console.log(`You still have ${toSee.length} movies to watch.`);
+
+      toSee.forEach((movie) => {
+        console.log(`You may watch ${movie.title} `);
+      });
+      readline.keyInPause();
+      break;
+    case 4:
+      console.log(`Movies watched`);
+      console.log(myCatalog);
+      readline.keyInPause();
+      break;
+    case 5:
+      exit = true;
+      break;
+    default:
+      console.log("Please choose a valid option!");
+  }
+}
